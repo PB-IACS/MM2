60cc5f7bdf692a2760a5248aa93c997edf050b44
Piotr <piotrbonecki66@gmail.com>
2026-03-27 11:52:38 +0100

    1

--- src/main/java/DBmanager/BaseMedhods/CategoryRepository.java
+++ src/main/java/DBmanager/BaseMedhods/CategoryRepository.java
@@ -1,5 +1,18 @@
 package DBmanager.BaseMedhods;
 
-public class CategoryRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import static DBmanager.Start.connect;
+
+public class CategoryRepository {
+    public static void addCategory(String name) throws SQLException {
+        String sql = "INSERT INTO Category(categoryName) VALUES(?)";
+        try (Connection conn = connect()) {//establish connection
+            PreparedStatement ps = conn.prepareStatement(sql);//through the connect pass sql argument
+            ps.setString(1, name);
+            ps.executeUpdate();
+        }
+    }
 }
